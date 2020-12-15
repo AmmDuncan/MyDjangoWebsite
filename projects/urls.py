f169ca65cad08f3af7cce345065eb4192bc89ae4
Krystian Maccs <krystianmaccs@gmail.com>
2020-12-15 03:02:04 -0800

    created a home.html template, modified a view and mapped it to an url

--- projects/urls.py
+++ projects/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.project_index, name="project_index"),
-    path("<int:pk>", views.project_detail, name="project_detail")
+
+    path("", views.home, name="home"),
+    path("<int:pk>", views.project_index, name="project_index"),
+    path("<int:pk>", views.project_detail, name="project_detail"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from projects.models import Project
 
 # Create your views here.
+def home(request):
+    return render(request, 'projects/home.html')
+
 def project_index(request):
     projects = Project.objects.all()
     context = {
